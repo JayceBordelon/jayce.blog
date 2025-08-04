34b235ab43f8c77ae1a596e961690e9dcaa06284
Jayce Bordelon <b.jayce@wustl.edu>
2025-08-04 13:59:39 -0500

    Seo

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -65,11 +65,24 @@ export async function generateMetadata({params}: { params: Promise<{ id: string 
             description: post.description,
             type: 'article',
             publishedTime: post.date,
+            images: [
+                {
+                    url: "https://www.wrc.noaa.gov/wrso/cartoons/SecRain.gif",
+                    width: 1200,
+                    height: 630,
+                    alt: post.title,
+                }
+            ]
         },
         twitter: {
             card: 'summary_large_image',
             title: post.title,
             description: post.description,
+            images: ["https://www.wrc.noaa.gov/wrso/cartoons/SecRain.gif"]
+        },
+
+        alternates: {
+            canonical: `/posts/${id}`,
         },
     };
 }
